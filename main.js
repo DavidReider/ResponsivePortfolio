86e52b04806c5584603d242b43c22368e3b84342
David Reider <57080304+DavidReider@users.noreply.github.com>
2020-09-25 23:17:02 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
+
+// main toggle
+menuBtn.addEventListener("click", () => {
+  toggle();
+});
+
+// toggle on item click if open
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
+});
+
+function toggle() {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
+//Animation
+const spans = document.querySelectorAll('h1 span')
+
+spans.forEach(index => index.addEventListener('mouseover', function(e){
+
+    index.classList.add('animated', 'rubberBand')
+} ))
+
+spans.forEach(index => index.addEventListener('mouseout', function(e){
+
+    index.classList.remove('animated', 'rubberBand')
+} ))
+
+
+function showRequiredCategory(event) {
+
+    const getId = event.id
+    const links = document.querySelectorAll('.work-category button')
+    for(i=0; i<links.length; i++){
+        if(links[i].hasAttribute('class')){
+            links[i].classList.remove('active')
+        }
+    }
+
+    event.classList.add('active')
+    const getCategory = document.querySelector(`.category-${getId}`)
+    const categories = document.querySelectorAll('div[class^= "category-"]')
+
+    for(i=0;i<categories.length; i++) {
+        if(categories[i].hasAttribute('class')){
+            categories[i].classList.remove('showCategory')
+            categories[i].classList.add('hideCategory')
+        }
+    }
+
+    getCategory.classList.remove('hideCategory')
+    getCategory.classList.add('showCategory')
+
+}
