bd0c9f8190f9752a6c4f257d0d391920d5f78b9d
DavidReider <freakdj1997@yahoo.com>
2022-01-01 21:44:12 -0500

    Code Cleanup

--- js/main.js
+++ js/main.js
@@ -22,41 +22,16 @@ function toggle() {
 }
 
 //Animation
-const spans = document.querySelectorAll('h1 span')
+const spans = document.querySelectorAll("h1 span");
 
-spans.forEach(index => index.addEventListener('mouseover', function(e){
+spans.forEach((index) =>
+  index.addEventListener("mouseover", function (e) {
+    index.classList.add("animated", "rubberBand");
+  })
+);
 
-    index.classList.add('animated', 'rubberBand')
-} ))
-
-spans.forEach(index => index.addEventListener('mouseout', function(e){
-
-    index.classList.remove('animated', 'rubberBand')
-} ))
-
-
-function showRequiredCategory(event) {
-
-    const getId = event.id
-    const links = document.querySelectorAll('.work-category button')
-    for(i=0; i<links.length; i++){
-        if(links[i].hasAttribute('class')){
-            links[i].classList.remove('active')
-        }
-    }
-
-    event.classList.add('active')
-    const getCategory = document.querySelector(`.category-${getId}`)
-    const categories = document.querySelectorAll('div[class^= "category-"]')
-
-    for(i=0;i<categories.length; i++) {
-        if(categories[i].hasAttribute('class')){
-            categories[i].classList.remove('showCategory')
-            categories[i].classList.add('hideCategory')
-        }
-    }
-
-    getCategory.classList.remove('hideCategory')
-    getCategory.classList.add('showCategory')
-
-}
+spans.forEach((index) =>
+  index.addEventListener("mouseout", function (e) {
+    index.classList.remove("animated", "rubberBand");
+  })
+);
