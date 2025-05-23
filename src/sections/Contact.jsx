7264ceaac12bdfb65c113616d4fd01b8a02be996
DavidReider <freakdj1997@yahoo.com>
2025-05-23 14:57:18 -0400

    Contact Section

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import Alert from "../components/Alert";
+import { Particles } from "../components/Particles";
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const showAlertMessage = (type, message) => {
+    setAlertType(type);
+    setAlertMessage(message);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      console.log("Form Submitted:", formData);
+      await emailjs.send(
+        "service_9f2gux7",
+        "template_bv4pl9w",
+        {
+          from_name: formData.name,
+          to_name: "DJ",
+          from_email: formData.email,
+          to_email: "reider_dj10@yahoo.com",
+          message: formData.message,
+        },
+        "78o42bA7vYZkq5lu-"
+      );
+      setIsLoading(false);
+      setFormData({ name: "", email: "", message: "" });
+      showAlertMessage("success", "You message has been sent!");
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      showAlertMessage("danger", "Somthing went wrong!");
+    }
+  };
+  return (
+    <section className="relative flex items-center c-space section-spacing">
+      <Particles
+        className="absolute inset-0 -z-50"
+        quantity={100}
+        ease={80}
+        color={"#ffffff"}
+        refresh
+      />
+      {showAlert && <Alert type={alertType} text={alertMessage} />}
+      <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="text-heading">Let's Talk</h2>
+          <p className="font-normal text-neutral-400">
+            Whether you're looking to build a new website, improve your existing
+            platform or bring a unique project to live, I'm here to help!
+          </p>
+        </div>
+        <form className="w-full" onSubmmit={handleSubmit}>
+          <div className="mb-5">
+            <label htmlFor="name" className="field-label">
+              Full Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="field-input field-input-focus"
+              placeholder="John Doe"
+              autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="email" className="field-label">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="field-input field-input-focus"
+              placeholder="JohnDoe@email.com"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="message" className="field-label">
+              Message
+            </label>
+            <textarea
+              type="text"
+              id="message"
+              name="message"
+              rows="4"
+              className="field-input field-input-focus"
+              placeholder="Share your thoughts..."
+              autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
+          >
+            {!isLoading ? "Submit" : "Submitting..."}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
